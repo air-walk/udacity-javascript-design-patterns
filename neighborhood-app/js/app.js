e89445a10adaf2dbed081ed569dcaa1268b5968e
Air Walk <air-walk@users.noreply.github.com>
2017-07-04 10:19:04 -0400

    adding map markers for 5 favorite locations

--- neighborhood-app/js/app.js
+++ neighborhood-app/js/app.js
@@ -1,9 +1,9 @@
 var locations = [
-  { name: "Neelam Chowk Ajronda", lat: 28.3976295, lng: 77.31232519999999 },
-  { name: "Faridabad Old",        lat: 28.410672,  lng: 77.31134          },
-  { name: "Badkhal Mor",          lat: 28.422814,  lng: 77.310278         },
+  { name: "Badarpur",             lat: 28.493498,  lng: 77.302969         },
   { name: "Sector 28",            lat: 28.4382752, lng: 77.30896249999999 },
-  { name: "Khan Market",          lat: 28.6020358, lng: 77.22922059999999 }
+  { name: "Badkhal Mor",          lat: 28.422814,  lng: 77.310278         },
+  { name: "Faridabad Old",        lat: 28.410672,  lng: 77.31134          },
+  { name: "Neelam Chowk Ajronda", lat: 28.3976295, lng: 77.31232519999999 }
 ];
 
 var Marker = function(data) {
@@ -43,25 +43,26 @@ var ViewModel = function() {
 
 }
 
-ko.applyBindings(new ViewModel());
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel);
 
 
 /* Callback method for rendering Google Map */
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map   = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: uluru
+  var markers = [];
+
+  viewModel.filteredMarkers().forEach(function(filteredMarker) {
+    markers.push({ lat: filteredMarker.lat(), lng: filteredMarker.lng() });
   });
 
-  /* var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: markers[0]
   });
 
-  var bulru = {lat: -25.363, lng: 101.044}
-  var marker2 = new google.maps.Marker({
-    position: bulru,
-    map: map
-  });*/
+  console.log(markers);
+  markers.forEach(function(marker) {
+    new google.maps.Marker({ position: marker, map: map });
+  });
 }
