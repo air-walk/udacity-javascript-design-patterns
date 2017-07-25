076b23693b7a435a81ed29a8e7b5985fac1e4b19
Air Walk <air-walk@users.noreply.github.com>
2017-07-25 18:43:24 -0400

    refactoring and making the code more lean

--- neighborhood-app/js/app.js
+++ neighborhood-app/js/app.js
@@ -1,5 +1,15 @@
-/* Variable to hold the map */
+/* Variable to hold the map and all markers */
 var map;
+var markers = [];
+
+/* Predefined locations in the neighborhood */
+var locations = [
+  { title: "Badarpur, Delhi",         position: { lat: 28.493498,  lng: 77.302969         }},
+  { title: "Sector 28 Metro Station", position: { lat: 28.4382752, lng: 77.30896249999999 }},
+  { title: "Badkhal Mor",             position: { lat: 28.422814,  lng: 77.310278         }},
+  { title: "Faridabad Old",           position: { lat: 28.410672,  lng: 77.31134          }},
+  { title: "Neelam Chowk Ajronda",    position: { lat: 28.3976295, lng: 77.31232519999999 }}
+];
 
 /* Wikipedia info */
 var loadWikiInfo = function(queryStr) {
@@ -33,74 +43,61 @@ var loadWikiInfo = function(queryStr) {
   });
 }
 
-/* Predefined locations in the neighborhood */
-var locations = [
-  { name: "Badarpur, Delhi",         lat: 28.493498,  lng: 77.302969         },
-  { name: "Sector 28 Metro Station", lat: 28.4382752, lng: 77.30896249999999 },
-  { name: "Badkhal Mor",             lat: 28.422814,  lng: 77.310278         },
-  { name: "Faridabad Old",           lat: 28.410672,  lng: 77.31134          },
-  { name: "Neelam Chowk Ajronda",    lat: 28.3976295, lng: 77.31232519999999 }
-];
-
-/* Marker representation */
-var Marker = function(data) {
-  this.name = ko.observable(data.name);
-  this.lat  = ko.observable(data.lat);
-  this.lng  = ko.observable(data.lng);
-}
-
 /* ViewModel for Knockout */
 var ViewModel = function() {
-  var self     = this;
-  this.markers = ko.observableArray([]);
-
-  locations.forEach(function(location) {
-    self.markers.push(new Marker(location));
-  });
-
-  this.currentMarker = ko.observable(this.markers()[0]);
-
-  this.setCurrentMarker = function(marker) {
-    self.currentMarker(marker);
-    loadWikiInfo(marker.name());
-  };
-
+  var self         = this;
+  self.vmMarkers   = ko.observableArray([]);
   this.searchQuery = ko.observable('');
+
   this.filteredMarkers = ko.computed(function() {
     var searchQueryStr = self.searchQuery().toLowerCase();
 
     if(!searchQueryStr) {
-      return self.markers();
+      return self.vmMarkers();
     } else {
-      return ko.utils.arrayFilter(self.markers(), function(marker) {
-        return marker.name().toLowerCase().indexOf(searchQueryStr) != -1;
+
+      return ko.utils.arrayFilter(self.vmMarkers(), function(marker) {
+        return marker.title.toLowerCase().indexOf(searchQueryStr) != -1;
       });
     }
   }, this);
+
 }
 
 /* Callback method for rendering Google Map */
 function initMap() {
+  /* Render the map */
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 28.422814,  lng: 77.310278 },
     zoom:   11
   });
 
-  /* Add markers to the map and bind click events for them */
-  viewModel.filteredMarkers().forEach(function(filteredMarker) {
-    var marker = new google.maps.Marker({ position: new google.maps.LatLng(filteredMarker.lat(), filteredMarker.lng()), map: map, title: filteredMarker.name()});
+  /* Add markers to the map and markers array, and bind click events for them */
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].position;
+    var title    = locations[i].title;
+
+    var marker = new google.maps.Marker({position: position, map: map, title: title, id: i});
+    markers.push(marker);
 
     marker.addListener('click', function() {
-      // console.log("Clicked " + marker.getTitle() + " at location: "+ marker.getPosition());
-      viewModel.setCurrentMarker(filteredMarker);
+      var thisMarker = this;
+      // console.log("Clicked " + thisMarker.getTitle() + " at location: "+ thisMarker.getPosition());
 
       // Make the selected marker bounce twice
-      marker.setAnimation(google.maps.Animation.BOUNCE);
+      thisMarker.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function() {
-        marker.setAnimation(null);
+        thisMarker.setAnimation(null);
       }, 1400);
+
+      // Diplay info from Wikipedia about this place
+      loadWikiInfo(thisMarker.title);
     });
-  });
+  }
+
+  for (var i = 0; i < markers.length; i++) {
+    viewModel.vmMarkers.push(markers[i]);
+  }
 }
 
 /* Applying bindings in Knockout */
